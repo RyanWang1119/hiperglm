ff892535f36d2524ec7375018e204fe3fcf5af6d
Ryan Wang <ewang35@jh.edu>
2025-01-29 01:18:00 -0500

    add the model_output

---
+++ R/hiper_glm.R
@@ -0,0 +1,17 @@
+
+#' @param outcome The outcome vector.
+#' @param design The design matrix.
+#' @return An object of class `hiperglm`.
+#' @export
+
+#'
+#' @examples
+
+#' # Example usage
+
+hiper_glm <- function(outcome, design) {
+  warning("Function hiper_glm is yet to be implemented.")
+  hglm_out <- list()
+  class(hglm_out) <- "hiperglm"
+  return(hglm_out)
+}
---
+++ R/model_output.R
@@ -0,0 +1,20 @@
+
+#' @export
+coef.hiperLogit <- function(object) {
+  warning("Function coef.hiperLogit is yet to be implemented.")
+  NULL
+}
+
+
+#' @export
+vcov.hiperLogit <- function(object) {
+  warning("Function vcov.hiperLogit is yet to be implemented.")
+  NULL
+
+}
+
+#' @export
+print.hiperLogit <- function(x, ...) {
+  warning("Function print.hiperLogit is yet to be implemented.")
+  cat("hiper_logitoutput\n")
+}
