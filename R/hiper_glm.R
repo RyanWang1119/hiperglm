e4752f0727840168aa20a544e1ac1f1e74021a7f
Ryan Wang <ewang35@jh.edu>
2025-01-30 14:08:41 -0500

    swith design outcome order

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -4,7 +4,7 @@
 #' @return An object of class `hiperglm`.
 #' @export
 
-hiper_glm <- function(outcome, design) {
+hiper_glm <- function(design, outcome) {
   warning("Function hiper_glm is yet to be implemented.")
   hglm_out <- list()
   class(hglm_out) <- "hiper_glm"
