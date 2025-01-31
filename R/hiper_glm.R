ad51e30c7b25cd674e517ce2dfd13d031fe78690
Ryan Wang <ewang35@jh.edu>
2025-01-31 02:14:08 -0500

    add testing

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -7,8 +7,14 @@
 hiper_glm <- function(outcome, design) {
   warning("Function hiper_glm is yet to be implemented.")
   hglm_out <- list()
-  class(hglm_out) <- "hiperglm"
+  class(hglm_out) <- "hiper_glm"
   return(hglm_out)
 }
 
-
+#a<- 2
+#b<- 1
+#model <- hiper_glm(a,b)
+#print(model)
+#print(a)
+#coef(model)
+#hiper_glm(a,b)
--- R/model_output.R
+++ R/model_output.R
@@ -2,20 +2,17 @@
 #' @export
 coef.hiper_glm <- function(object) {
   warning("Function coef.hiper_glm is yet to be implemented.")
-  NULL
 }
 
 
 #' @export
 vcov.hiper_glm <- function(object) {
   warning("Function vcov.hiper_glm is yet to be implemented.")
-  NULL
 
 }
 
 #' @export
 print.hiper_glm <- function(x, ...) {
-  warning("Function print.hiper_glm is yet to be implemented.")
   cat("hiper_glm output\n")
 }
 
